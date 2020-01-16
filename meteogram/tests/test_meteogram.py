a72e7a97e0bcdf4f2fd6d2af6a42e0310ea96b89
Etienne Charlier <bitbucket@procurasia.com>
2020-01-16 11:06:13 +0800

    Rune a few tests from basics

--- meteogram/tests/test_meteogram.py
+++ meteogram/tests/test_meteogram.py
@@ -1,5 +1,6 @@
 """Test use of the meteogram module."""
 
+import datetime
 from meteogram import meteogram
 
 
@@ -25,6 +26,18 @@ def test_degF_to_degC_at_freezing():
 #
 # Instructor led introductory examples
 #
+def test_title_case():
+    # Setup
+    input = 'this is a test string'
+    desired = 'This Is A Test String'
+
+    # Exercise
+    actual = input.title()
+
+    # Verify
+    assert actual == desired
+
+    # Cleanup - none necessary
 
 #
 # Instructor led examples of numerical comparison
@@ -37,7 +50,29 @@ def test_build_asos_request_url_single_digit_datetimes():
     """
     Test building URL with single digit month and day.
     """
-    pass
+    # Setup
+    start = datetime.datetime(2018, 1, 5, 1)
+    end = datetime.datetime(2018, 1, 9, 1)
+    station = 'FSD'
+
+    # Exercise
+    result_url = meteogram.build_asos_request_url(station, start, end)
+
+    # Verify
+    # tip: first try the test with an empty truth_url,
+    #      then run and see the result_url pytest returns (calculated one)
+    #      test truth_url in the browser and verify it is correct
+    #      update the truth_url with the correct value
+    truth_url = ('https://mesonet.agron.iastate.edu/request/asos/1min_dl.php?'
+                 'station%5B%5D=FSD&tz=UTC&year1=2018&month1=01&day1=05&hour1=01'
+                 '&minute1=00&year2=2018&month2=01&day2=09&hour2=01&minute2=00&'
+                 'vars%5B%5D=tmpf&vars%5B%5D=dwpf&vars%5B%5D=sknt&vars%5B%5D=drct&'
+                 'sample=1min&what=view&delim=comma&gis=yes')
+    assert result_url == truth_url
+
+    # Cleanup - none necessary
+
+
 
 
 def test_build_asos_request_url_double_digit_datetimes():
